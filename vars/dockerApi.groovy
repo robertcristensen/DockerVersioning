557d22c4785e1a90ef97304b05de33a17aaeb88d
Viktor Mikula <viktor.mikula@genesys.com>
2017-11-29 16:26:20 +0200

    added

--- vars/dockerApi.groovy
+++ vars/dockerApi.groovy
@@ -1,15 +1,14 @@
 #!/usr/bin/env groovy
 import com.pipeline.libs.DockerRegistry
 
-def call(){
+def call(String name='haha'){
 /*
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
 */
-    echo "Hello from my lib"
-    def r = new DockerRegistry(this)
-    r.connect('bsregistrydocker.emea.int.genesyslab.com/genesys/cx-widget:default')
+    echo "Hello from my lib ${haha}"
+
 }
 
