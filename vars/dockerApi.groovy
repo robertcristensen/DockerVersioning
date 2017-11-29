d9846cf1706abb0e2ab80d5662937b98dba9e4be
Viktor Mikula <viktor.mikula@genesys.com>
2017-11-29 16:45:32 +0200

    added

--- vars/dockerApi.groovy
+++ vars/dockerApi.groovy
@@ -1,17 +1,12 @@
 #!/usr/bin/env groovy
 import com.pipeline.libs.DockerRegistry
 
-def call(def img){
-/*
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-*/
-    echo "Hello from my lib"
+def call(def img, int n){
+
+    echo "Running Docker Registry cleaning procedure"
     def r = new DockerRegistry(this)
     r.connect(img)
-    r.purge(3)
+    r.purge(n)
 
 }
 
