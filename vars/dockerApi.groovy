f00b3ef2ddc5e18d926c126cb9b8a9c03cde942e
Viktor Mikula <viktor.mikula@genesys.com>
2017-11-29 14:23:08 +0200

    added

---
+++ vars/dockerApi.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+import com.pipeline.libs.DockerRegistry
+
+def call(body){
+
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    def r = new DockerRegistry(config.this)
+}
+
